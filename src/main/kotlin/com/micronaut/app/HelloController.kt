15bbfdd3e2ac5d72ec8becbaf16eefff7b6514b6
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-28 21:56:47 +0530

    Add HelloController with @Get mapping example.

---
+++ src/main/kotlin/com/micronaut/app/HelloController.kt
@@ -0,0 +1,12 @@
+package com.micronaut.app
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+
+@Controller("/hello")
+class HelloController {
+    @Get
+    fun index() = "Hello World"
+}
