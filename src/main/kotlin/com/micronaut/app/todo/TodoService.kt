9e9a7c8dfaad8f9d6d0a673730ad2d299985742f
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-29 08:55:18 +0530

    Add Service Layer for CRUD operations.

---
+++ src/main/kotlin/com/micronaut/app/todo/TodoService.kt
@@ -0,0 +1,9 @@
+package com.micronaut.app.todo
+
+interface TodoService {
+    fun listAll(): List<Todo>
+    fun getById(id: Long): Todo
+    fun create(todo: Todo): Todo
+    fun updateById(id: Long, todo: Todo): Todo
+    fun deleteById(id: Long)
+}
---
+++ src/main/kotlin/com/micronaut/app/todo/TodoServiceImpl.kt
@@ -0,0 +1,28 @@
+package com.micronaut.app.todo
+
+import jakarta.inject.Singleton
+
+@Singleton
+class TodoServiceImpl(
+    private val todoRepository: TodoRepository
+): TodoService {
+    override fun listAll(): List<Todo> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getById(id: Long): Todo {
+        TODO("Not yet implemented")
+    }
+
+    override fun create(todo: Todo): Todo {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateById(id: Long, todo: Todo): Todo {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(id: Long) {
+        TODO("Not yet implemented")
+    }
+}
