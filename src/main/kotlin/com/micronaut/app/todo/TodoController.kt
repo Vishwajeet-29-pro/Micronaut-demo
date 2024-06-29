ef58125985fde8202c8acfc8fc12f9eb1b88e471
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-29 13:13:30 +0530

    Add TodoController class and create CRUD operations.

---
+++ src/main/kotlin/com/micronaut/app/todo/TodoController.kt
@@ -0,0 +1,35 @@
+package com.micronaut.app.todo
+
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Put
+import jakarta.inject.Inject
+
+@Controller("/api/todo")
+class TodoController(@Inject private val todoService: TodoService) {
+
+    @Get("/")
+    fun listAllTodo(): List<Todo> = todoService.listAll()
+
+    @Get("/{id}")
+    fun getTodoById(@PathVariable id: Long): Todo? {
+        return todoService.getById(id)
+    }
+
+    @Post("/")
+    fun createTodo(@Body todo: Todo): Todo {
+        return todoService.create(todo)
+    }
+
+    @Put("/{id}")
+    fun updateTodo(@PathVariable id: Long, @Body todo: Todo): Todo {
+        return todoService.updateById(id, todo)
+    }
+
+    @Delete("/{id}")
+    fun deleteById(@PathVariable id: Long) = todoService.deleteById(id)
+}
