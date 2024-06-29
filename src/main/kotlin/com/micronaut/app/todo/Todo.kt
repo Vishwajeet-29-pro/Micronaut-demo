14f09a65f121a7de9c13f318bc3bf70dc331a719
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-29 22:17:02 +0530

    Fix error of deserializable.
    Internal Server Error: No deserializable introspection present for type: Todo todo. Consider adding Serdeable.Deserializable annotate to type Todo todo. Alternatively if you are not in control of the project's source code. \n - By Adding @Serdeable annotation on data class.

--- src/main/kotlin/com/micronaut/app/todo/Todo.kt
+++ src/main/kotlin/com/micronaut/app/todo/Todo.kt
@@ -1,9 +1,11 @@
 package com.micronaut.app.todo
 
+import io.micronaut.serde.annotation.Serdeable
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 
+@Serdeable
 @Entity
 data class Todo(
     @Id
